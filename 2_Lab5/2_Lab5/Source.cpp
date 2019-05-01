1a271fb167f4e8772969df07fa73e231752064bd
JacksonJH <nobpok228@mail.ru>
2019-05-02 01:37:16 +0300

    roots of equations

--- 2_Lab5/2_Lab5/Source.cpp
+++ 2_Lab5/2_Lab5/Source.cpp
@@ -5,95 +5,145 @@
 
 using namespace std;
 
-struct Stack {
-	char info;
-	Stack *prev, *next;
-} *beginQueue, *endQueue;
+const int SIZE = 81;
+double arr[201];
 
-double calcAvgQueue(Stack *);
-Stack* rmAvgQueue(Stack *, int &);
-double readIntNum();
-void viewFromBegin(Stack *);
-void viewFromEnd(Stack *);
+struct Stack {
+	char c;
+	Stack  *next;
+} *op;
+
+double readDoubleNum();
+int priority(char);
+Stack* inStack(Stack*, char);
+Stack* outStack(Stack*, char*);
+double result(char*);
 
 int main() {
-	srand(time(NULL));
+	Stack *t;
+	op = NULL;
+	char a, in[SIZE], out[SIZE];
 
-	int stop = 0;
-	do {
-		cout <<
-			"Instruction:\n"
-			" 1 to use default value: a*(b+c)/(d-e) with a=0.4, b=2.3, c=6.7, d=5.8, e=9.1\n"
-			" 2 to enter own values\n"
-			" 5 to exit\n"
-			"Enter: "
-			;
-		int choice = readIntNum();
-		string inputStr;
-		switch (choice) {
-		case 1:
-		{
-			inputStr = "a*(b+c)/(d-e)";
-			cout << inputStr;
-			system("pause");
-			Stack *t;
-			while (choice--) {
-				t = new Stack;
-				//t->info = rand() % (b - a + 1) + a;
-				t->next = NULL;
-				if (beginQueue == NULL) {
-					t->prev = NULL;
-					beginQueue = endQueue = t;
-					continue;
-				}
-				t->prev = endQueue;
-				endQueue->next = t;
-				endQueue = t;
-			}
+	int k = 0, l = 0;
+	cout << "Input formula: ";
+	cin >> in;
 
-			viewFromBegin(beginQueue);
-			system("pause");
-			break;
-		}
-		case 2:
-		{
-			getline(cin, inputStr);
-			cout << inputStr;
-			system("pause");
+	while (in[k] != '\0') {
+		if (in[k] >= 'a' && in[k] <= 'z')
+			out[l++] = in[k];
 
-			Stack *t = new Stack;
-			//t->info = ????;
-			if (beginQueue == NULL) {
-				t->next = NULL;
-				t->prev = NULL;
-				beginQueue = endQueue = t;
+		if (in[k] == '(')
+			op = inStack(op, in[k]);
+
+		if (in[k] == ')') {
+			while ((op->c) != '(') {
+				op = outStack(op, &a);
+				if (!op) a = '\0';
+				out[l++] = a;
 			}
-			else if (choice == 1) {
-				t->prev = NULL;
-				t->next = beginQueue;
-				beginQueue->prev = t;
-				beginQueue = t;
-			}
-			else {
-				t->prev = endQueue;
-				t->next = NULL;
-				endQueue->next = t;
-				endQueue = t;
-			}
-			break;
+			t = op;
+			op = op->next;
+			delete t;
 		}
-		case 5:
-			stop = 1;
-			break;
+		if (in[k] == '+' || in[k] == '-' || in[k] == '*' || in[k] == '/') {
+			while (op != NULL && priority(op->c) >= priority(in[k])) {
+				op = outStack(op, &a);
+				out[l++] = a;
+			}
+			op = inStack(op, in[k]);
 		}
-		system("cls");
-	} while (!stop);
+		k++;
+	}
+
+	while (op != NULL) {
+		op = outStack(op, &a);
+		out[l++] = a;
+	}
+	out[l] = '\0';
+	cout << "Polish:\n" << out << '\n';
+
+	cout << "Input a: ";
+	double userA = readDoubleNum();
+	cout << "Input b: ";
+	double userB = readDoubleNum();
+	cout << "Input c: ";
+	double userC = readDoubleNum();
+	cout << "Input d: ";
+	double userD = readDoubleNum();
+	cout << "Input e: ";
+	double userE = readDoubleNum();
+
+	arr[int('a')] = userA;
+	arr[int('b')] = userB;
+	arr[int('c')] = userC;
+	arr[int('d')] = userD;
+	arr[int('e')] = userE;
+	//}
+	cout << "Result is " << result(out) << '\n';
+
+	system("pause");
+}
+int priority(char a) {
+	switch (a) {
+	case '*':
+	case '/': return 3;
+
+	case '-':
+	case '+': return 2;
+
+	case '(': return 1;
+	}
 
 	return 0;
 }
-double readIntNum() {
+Stack* inStack(Stack *p, char s) {
+	Stack *t = new Stack;
+	t->c = s;
+	t->next = p;
+	return t;
+}
+Stack* outStack(Stack *p, char *s) {
+	Stack *t = p;
+	*s = p->c;
+	p = p->next;
+	delete t;
+	return p;
+}
+double result(char* str) {
+	char ch, ch1, ch2, chr;
+	double op1, op2, rez;
+
+	chr = 'z' + 1;
+	for (int i = 0; i <= strlen(str); i++) {
+		ch = str[i];
+		if (!(ch == '+' || ch == '-' || ch == '*' || ch == '/')) {
+			op = inStack(op, ch);
+		}
+		else {
+			op = outStack(op, &ch1);
+			op = outStack(op, &ch2);
+
+			op1 = arr[int (ch1)];//
+			op2 = arr[int (ch2)];//
+
+			switch (ch)
+			{
+			case '+': rez = op2 + op1; break;
+			case '-': rez = op2 - op1; break;
+			case '*': rez = op2 * op1; break;
+			case '/': rez = op2 / op1; break;
+			case '^': rez = pow(op2, op1); break;
+			}
+			arr[int(chr)] = rez;//
+			op = inStack(op, chr);
+			chr++;
+		}
+	}
+	return rez;
+}
+double readDoubleNum() {
 	while (1) {
-		int num;
+		double num;
 		cin >> num;
 		if (cin.get() == '\n') {
 			return num;
@@ -101,19 +151,7 @@ double readIntNum() {
 		else {
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			cout << " Error 1: Value should be type int.\n ";
+			cout << " Error 1: Value should be type double.\n ";
 		}
-	}
-}
-void viewFromBegin(Stack *b) {
-	Stack *t = b;
-	for (int i = 1; t != NULL; i++, t = t->next) {
-		cout << i << " - " << t->info << '\n';
-	}
-}
-void viewFromEnd(Stack *e) {
-	Stack *t = e;
-	for (int i = 1; t != NULL; i++, t = t->prev) {
-		cout << i << " - " << t->info << '\n';
 	}
 }
--- 2_Lab6/2_Lab6/Source.cpp
+++ 2_Lab6/2_Lab6/Source.cpp
@@ -1,31 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <conio.h>
 #include <ctime>
+#include <math.h> 
 
 using namespace std;
 
-int main() {
-	srand(time(NULL));
+double fun(double);
+double diff_fun(double);
+char *newthonMethod(double i, double e);
+double readDoubleNum();
+int readIntNum();
 
-	int stop = 0;
-	do {
-		cout <<
-			"Instruction:\n"
-			" 5 to exit\n"
-			"Enter: "
-			;
-		int choice = readIntNum();
-		switch (choice) {
-		case 5:
-			stop = 1;
-			break;
+int main()
+{
+	cout << "Enter interval [a; b]:\n";
+	int a = readIntNum();
+	int b = readIntNum();
+	cout << "e(initial approximation):\n";
+	double e = readDoubleNum();
+
+	double *arr = new double[6];
+	int j = 0;
+	char *res = new char[30];
+	for (int i = a; i <= b; i += 2)
+	{
+		if (strcmp((res = newthonMethod(i, e)), "-")) {
+			arr[j++] = atof(res);
 		}
-		system("cls");
-	} while (!stop);
+	}
+	
+	cout << arr[0] << '\n';
+	cout << arr[1] << '\n';
+	cout << arr[2] << '\n';
 
-	return 0;
+	system("pause");
 }
-double readIntNum() {
+char *newthonMethod(double x, double e) {
+	double h;
+	int i = 0;
+	char *st = new char[10];
+	do {
+		h = fun(x) / diff_fun(x);
+		if (fabs(h) <= e)
+			return _gcvt(x, 10, st);
+		x -= h;
+	} while (++i < 10);
+	strcpy(st, "-");
+	return st;
+}
+double fun(double x) {
+	return pow(x, 3) - 5 * pow(x, 2) + 12;
+}
+double diff_fun(double x) {
+	return 3 * pow(x, 2) - 10 * x;
+}
+double readDoubleNum() {
+	while (1) {
+		double num;
+		cin >> num;
+		if (cin.get() == '\n') {
+			return num;
+		}
+		else {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << " Error 1: Value should be type double.\n ";
+		}
+	}
+}
+int readIntNum() {
 	while (1) {
 		int num;
 		cin >> num;
